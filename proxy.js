a3dea3093c2ed95a3057cb7bbac5c8ad1a06711c
= <mtondelli1420@gmail.com>
2020-06-04 21:30:41 +0200

    Moved to standard resolvers

--- proxy.js
+++ proxy.js
@@ -28,56 +28,22 @@ var proxySettings = {
 	resolvers: []
 };
 
-//	#################
-//	### Resolvers ###
-//	#################
-// Create a custom resolver to a path, and then set the priority to 100
-
-//	### Development resolver ###
-proxySettings.resolvers[0] = function (host, url, req) {
-	if (/^\/dev\//.test(url)) {
-		return 'http://172.16.100.1';
-	}
-}
-proxySettings.resolvers[0].priority = 100;	//	/dev -> dev
-//	### Paperless resolver ###
-proxySettings.resolvers[1] = function (host, url, req) {
-	if (/^\/paper\//.test(url)) {
-		return 'http://172.16.100.2';
-	}
-}
-proxySettings.resolvers[1].priority = 100;	//	/paper -> paperless
-//	### Terrairia resolver ###
-proxySettings.resolvers[2] = function (host, url, req) {
-	if (/^\/terraria\//.test(url)) {
-		return 'http://172.16.100.3';
-	}
-}
-proxySettings.resolvers[2].priority = 100;	//	/terraria -> terraria
-//	### Torrent resolver ###
-proxySettings.resolvers[3] = function (host, url, req) {
-	if (/^\/torrent\//.test(url)) {
-		return 'http://172.16.100.4';
-	}
-}
-proxySettings.resolvers[3].priority = 100;	//	/torrent -> qbittorrent
-//	### Share resolver ###
-proxySettings.resolvers[4] = function (host, url, req) {
-	if (/^\/share\//.test(url)) {
-		return 'http://172.16.100.36';
-	}
-}
-proxySettings.resolvers[4].priority = 100;	//	/share -> smb
-//	### Nextcloud resolver ###
-proxySettings.resolvers[5] = function (host, url, req) {
-	if (/^\/nextcloud\//.test(url)) {
-		return 'http://172.16.100.6';
-	}
-}
-proxySettings.resolvers[5].priority = 100;	//	/nextcloud -> nextcloud
-
 //	######################
 //	### Initialization ###
 //	######################
 
 var proxy = require('redbird')(proxySettings);
+
+//	#################
+//	### Resolvers ###
+//	#################
+// Create a custom resolver to a path, and then set the priority to 100
+
+proxy.register("neet.ddns.net/", "192.168.10.10:8080");
+proxy.register("neet.ddns.net/dev", "192.168.10.20");
+proxy.register("neet.ddns.net/paper", "192.168.10.21");
+proxy.register("neet.ddns.net/terraria", "192.168.10.22");
+proxy.register("neet.ddns.net/torrent", "192.168.10.23");
+proxy.register("neet.ddns.net/share", "192.168.10.24");
+proxy.register("neet.ddns.net/docker", "192.168.10.25");
+proxy.register("neet.ddns.net/nextcloud", "192.168.10.26");
